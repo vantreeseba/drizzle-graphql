ae0e737fe5926be17e8b96fc39f2440e10b9cfff
Benjamin Van Treese <vantreeseba@gmail.com>
2026-03-10 22:00:47 -0500

    fix: Handle case where input undefined in case ops.

--- src/util/case-ops/index.ts
+++ src/util/case-ops/index.ts
@@ -1,12 +1,12 @@
 import pluralize from 'pluralize';
 
 export const uncapitalize = <T extends string>(input: T) =>
-  (input.length
+  (input?.length
     ? `${input[0]!.toLocaleLowerCase()}${input.length > 1 ? input.slice(1, input.length) : ''}`
     : input) as Uncapitalize<T>;
 
 export const capitalize = <T extends string>(input: T) =>
-  (input.length
+  (input?.length
     ? `${input[0]!.toLocaleUpperCase()}${input.length > 1 ? input.slice(1, input.length) : ''}`
     : input) as Capitalize<T>;
 
